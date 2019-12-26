7d87d0fae4fee7ecf8b6a79d15e473a05a5e3146
Chandra Bhushan <swiftserieschannel@gmail.com>
2019-12-26 15:56:14 +0530

    Update createCustomer.php

--- createCustomer.php
+++ createCustomer.php
@@ -6,10 +6,10 @@ require_once('./stripe-php/init.php');
 \Stripe\Stripe::setApiKey('sk_test_nPdYeoBLMmIE4lYcuX0ixLc700aonA2sCZ');
 
 
-$email =  $_GET['email'] ;
+$email =  $_POST['email'] ;
 $fullName = $_POST['name'];
 $phone	= $_POST['phone'];
-echo $email.$fullName;
+
 $key = \Stripe\Customer::create([
   'description' => 'testing','email'=>"$email",'phone'=>"$phone", 'name'=>$fullName
 ]);
